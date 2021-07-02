16f06419ef0965e47415920a72be12a44506c45d
Keegan <keegan@twinoaks.org>
2021-07-02 16:53:22 -0400

    HERE word defined.

--- ForschTests/Tests.cs
+++ ForschTests/Tests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace ForcshTests
 {
@@ -8,8 +10,15 @@ namespace ForcshTests
         [Test]
         public void CompileTests()
         {
-            var preCompiled = "BLAH BLAH BEGIN 1 + DUP 10 = UNTIL";
-            //Assert.AreEqual("BLAH BLAH BEGIN 1 + DUP 10 = BRANCHF 2", Forsch.
+            var replacements = new Dictionary<String, String>
+            {
+                ["alpha"] = "beta",
+                ["gamma"] = "delta"
+            };
+            Assert.AreEqual("beta 1 delta 2", Forsch.Program.DictionaryReplace(replacements, "alpha 1 gamma 2"));
+            Assert.AreEqual("zeta eta theta", Forsch.Program.DictionaryReplace(replacements, "zeta eta theta"));
+            //var preCompiled = "BLAH BLAH BEGIN 1 + DUP 10 = UNTIL";
+            //Assert.AreEqual("BLAH BLAH BEGIN 1 + DUP 10 = BRANCHF 2", Forsch.Program.Compile(preCompiled));
         }
     }
 }
